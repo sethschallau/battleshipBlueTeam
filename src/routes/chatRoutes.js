f49fa4bb7501c9a9702a933f70bddd3f14218cce
gkjenkin <gkjenkin@ncsu.edu>
2023-04-16 16:33:10 -0400

    working on different chat

--- src/routes/chatRoutes.js
+++ src/routes/chatRoutes.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const chatController = require('../controllers/chatController');
+const { Server } = require('socket.io');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 const router = express.Router();
+
+
 
 router.get('/get/:id', chatController.getChats);
 
 router.post('/send/:id', chatController.sendChat);
+
 
 module.exports = router;
