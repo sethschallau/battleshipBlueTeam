2c6d5a7acc86ced59e2d7a99aceed0975682254f
Seth Schallau <sethmschallau@gmail.com>
2023-04-18 22:12:06 -0400

    sockets for chat done, I think the game works, but can't test yet. chatbox had to be modified for new information to display, cdx wasn't playing nice

--- src/app/chat-box/chat-box.component.ts
+++ src/app/chat-box/chat-box.component.ts
@@ -1,72 +1,73 @@
-import { Component, OnInit, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ScrollingModule } from '@angular/cdk/scrolling';
+import { io, Socket } from 'socket.io-client';
 
 @Component({
   selector: 'app-chat-box',
   templateUrl: './chat-box.component.html',
   styleUrls: ['./chat-box.component.css'],
 })
-export class ChatBoxComponent implements AfterViewInit {
-
+export class ChatBoxComponent implements OnInit, OnDestroy {
   messages: any[] = [];
   newMessage: string = '';
   gameId: string = '';
-  CloseChat:boolean = true;
-  chatIsVisible:boolean = true;
-  items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
+  CloseChat: boolean = true;
+  chatIsVisible: boolean = true;
+  items = Array.from({ length: 100000 }).map((_, i) => `Item #${i}`);
+  private socket: any;
 
-  constructor(private http: HttpClient, private elementRef: ElementRef) { }
+  constructor(private http: HttpClient, private elementRef: ElementRef) {
+    this.socket = io('http://localhost:3000');
+  }
 
-  ngAfterViewInit(): void {
-    // Wait for DOM to be ready before retrieving gameId from the DOM
+  ngOnInit(): void {
     setTimeout(() => {
-      //the same thing as the username comment below
       this.gameId = this.elementRef.nativeElement.querySelector('#gameId').textContent;
-      this.http
-        .get<any[]>(`http://localhost:3000/chats/get/${this.gameId}`)
-        .subscribe((data: any) => (this.messages = data.chats));
+      this.socket.emit('getChats', this.gameId);
+
+      this.socket.on('chatData', (data: any) => {
+        this.messages = data.chats;
+      });
+
+      // Listen for new messages from the server
+      this.socket.on('message', (newMessage: any) => {
+        this.messages.push(newMessage);
+      });
     }, 0);
   }
 
+  ngOnDestroy(): void {
+    this.socket.disconnect();
+  }
+
   sendMessage(imageName: string): void {
-    this.http
-      .post(`http://localhost:3000/chats/send`, {
-        //you need to put the username of the current player in a DOM item with the id #yourUserName
-        //needs to just grab from state later
-        playerUserName: this.elementRef.nativeElement.querySelector('#yourUserName').textContent,
-        imageFile: imageName,
-        note: "NA",
-        gameId: this.gameId
-      })
-      .subscribe(() => {
-        this.ngAfterViewInit();
-      });
+    this.socket.emit('newMessage', {
+      playerUserName: this.elementRef.nativeElement.querySelector('#yourUserName').textContent,
+      imageFile: imageName,
+      note: 'NA',
+      gameId: this.gameId
+    });
 
     this.newMessage = '';
   }
 
   sendTextMessage(): void {
-    this.http
-      .post('http://localhost:3000/chats/send', {
-        playerUserName: this.elementRef.nativeElement.querySelector('#yourUserName').textContent,
-        imageFile: "NA",
-        note: this.newMessage,
-        gameId: this.gameId
-      })
-      .subscribe(() => {
-        this.ngAfterViewInit();
-      });
+    this.socket.emit('newMessage', {
+      playerUserName: this.elementRef.nativeElement.querySelector('#yourUserName').textContent,
+      imageFile: 'NA',
+      note: this.newMessage,
+      gameId: this.gameId
+    });
 
-      this.newMessage = '';
-
+    this.newMessage = '';
   }
 
-  closeForm(): void{
+  closeForm(): void {
     this.CloseChat = !this.CloseChat;
     this.chatIsVisible = !this.chatIsVisible;
   }
-  openForm(): void{
+
+  openForm(): void {
     this.chatIsVisible = !this.chatIsVisible;
   }
 }
