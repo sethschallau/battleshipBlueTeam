ccd529496e0987bee5a605fa60c202ba345d5877
gkjenkin <gkjenkin@ncsu.edu>
2023-04-22 15:17:59 -0400

    Updated styling for homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,8 @@ import { CurrentGamesComponent } from './current-games/current-games.component';
 import { ChatBoxComponent } from './chat-box/chat-box.component';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { MatRadioModule } from '@angular/material/radio';
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 @NgModule({
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
 
