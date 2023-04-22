cb16d7d73e108fc76c677d84e22f35334c2b5c71
gkjenkin <gkjenkin@ncsu.edu>
2023-04-22 17:30:56 -0400

    Chat now shows usernames but sends extra empty msg

--- src/app/chat-box/chat-box.component.ts
+++ src/app/chat-box/chat-box.component.ts
@@ -2,6 +2,18 @@ import { Component, OnInit, OnDestroy, ElementRef, AfterViewChecked, ViewChild }
 import { HttpClient } from '@angular/common/http';
 import { io, Socket } from 'socket.io-client';
 
+
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { Game } from '../_models/game';
+import { environment } from 'src/environments/environment';
+import { User } from '../_models/user';
+import { BoardService } from 'src/app/game/board-service.service';
+import { AccountService } from '../_security/account.service';
+
+
+const NUM_PLAYERS: number = 2;
+
 @Component({
   selector: 'app-chat-box',
   templateUrl: './chat-box.component.html',
@@ -19,6 +31,16 @@ export class ChatBoxComponent implements OnInit, OnDestroy {
   ggImgSrc = '../assets/emoticons/GG.png';
   wowImgSrc = '../assets/emoticons/Wow.png';
 
+  // Chat id stuff
+  idParamSubscription: Subscription;
+  apiUrl: string = environment.apiUrl
+  canPlay: boolean = false;
+  player: number = 0;
+  players: number = 0;
+  game: Game;
+  user: User;
+  username: string;
+
 
   /** The scrollable chat view */
   @ViewChild('scrollMe') private myScrollContainer: ElementRef;
@@ -26,13 +48,24 @@ export class ChatBoxComponent implements OnInit, OnDestroy {
   /** The input for the form */
   @ViewChild('messageInput') messageInput: ElementRef;
 
-  constructor(private http: HttpClient, private elementRef: ElementRef) {
+  constructor(private http: HttpClient, 
+    private elementRef: ElementRef, 
+    private route: ActivatedRoute,
+    private boardService: BoardService,
+    private accountService: AccountService
+
+    ) {
     this.socket = io('http://localhost:3000');
+    let checkUser = this.accountService.currentUserValue;
+    if (checkUser) {
+      this.user = checkUser;
+      this.username = this.user.username;
+   }
   }
 
   ngOnInit(): void {
     setTimeout(() => {
-      this.gameId = this.elementRef.nativeElement.querySelector('#gameId').textContent;
+      this.gameId = this.gameId;
       this.socket.emit('getChats', this.gameId);
 
       this.socket.on('chatData', (data: any) => {
@@ -44,6 +77,13 @@ export class ChatBoxComponent implements OnInit, OnDestroy {
         this.messages.push(newMessage);
       });
     }, 0);
+
+
+    this.idParamSubscription = this.route.params.subscribe(
+      params => {
+        this.getGame(params['id'])
+      }
+  )
   }
 
   ngOnDestroy(): void {
@@ -56,7 +96,7 @@ export class ChatBoxComponent implements OnInit, OnDestroy {
 
   sendMessage(imageName: string): void {
     this.socket.emit('newMessage', {
-      playerUserName: this.elementRef.nativeElement.querySelector('#yourUserName').textContent,
+      playerUserName: this.user.username,
       imageFile: imageName,
       note: 'NA',
       gameId: this.gameId
@@ -67,7 +107,7 @@ export class ChatBoxComponent implements OnInit, OnDestroy {
 
   sendTextMessage(): void {
     this.socket.emit('newMessage', {
-      playerUserName: this.elementRef.nativeElement.querySelector('#yourUserName').textContent,
+      playerUserName: this.user.username,
       imageFile: 'NA',
       note: this.newMessage,
       gameId: this.gameId
@@ -95,7 +135,7 @@ export class ChatBoxComponent implements OnInit, OnDestroy {
     console.log(data);
     // alert("The input entered is: " + data.input);
     this.socket.emit('newMessage', {
-      playerUserName: this.elementRef.nativeElement.querySelector('#yourUserName').textContent,
+      playerUserName: this.user.username, //this.elementRef.nativeElement.querySelector('#yourUserName').textContent,
       imageFile: 'NA',
       note: data.input,
       gameId: this.gameId
@@ -103,4 +143,37 @@ export class ChatBoxComponent implements OnInit, OnDestroy {
     
     this.messageInput.nativeElement.value = '';
   }
+
+
+  getGame(id: number) {
+    var httpRequest = this.http.get<Game>(`${this.apiUrl}/games/${id}`)
+
+        httpRequest.subscribe(
+            returnedGame => {
+                this.game = returnedGame;
+                this.gameId = this.game._id;
+                this.players = this.game.players.length;
+                // this.updateCurrentPlayer();
+                // this.createBoards();
+            })
+  }
+
+  // updateCurrentPlayer(): void {
+  //   if (this.game.currentPlayer.includes(this.username)) {
+  //     this.canPlay = true;
+  //   } else {
+  //     this.canPlay = false;
+  //   }
+  // }
+
+  // createBoards() : ChatBoxComponent {
+  //   this.boardService.createBoard(this.user, this.game);
+  //   for (let p of this.game.players) {
+  //     if (!p.username.includes(this.username)) {
+  //       this.boardService.createBoard(p, this.game);
+  //     }
+  //   }
+    
+  //   return this;
+  // }
 }
